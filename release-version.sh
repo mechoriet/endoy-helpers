1390c00ce9c01ddb2c9240530ecb5f6941b097e2
Dieter Blancke <dieter.blancke@outlook.com>
2024-07-12 23:28:17 +0200

    Added .bat version

--- release-version.sh
+++ release-version.sh
@@ -1,10 +1,12 @@
 tag=$1
 
+git checkout master
+
 ./mvnw -B versions:set -DnewVersion=$tag -DgenerateBackupPoms=false
 
 git add .
 git commit -m "Release version $tag"
-git push origin master
+git push
 
 git tag v$tag
 git push origin v$tag
