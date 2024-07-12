604fec6fc20d4ad8a9d54e0bed836275abb6c441
Dieter Blancke <dieter.blancke@outlook.com>
2024-07-12 23:34:09 +0200

    Release version v0.0.34

--- release-version.bat
+++ release-version.bat
@@ -2,6 +2,7 @@
 
 SET "tag=%~1"
 git "checkout" "master"
+CALL ./mvnw.cmd "-B" "versions:set" "-DnewVersion=%tag%" "-DgenerateBackupPoms=false"
 git "add" "."
 git "commit" "-m" "Release version v%tag%"
 git "push"
